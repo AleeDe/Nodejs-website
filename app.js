b99548397e13487797a1cf1ded01efab83ef4d61
Muhammad Ali <122118041+AleeDe@users.noreply.github.com>
2024-02-03 02:25:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/static',express.static('static'));
 app.set('view engine', 'pug');
 app.set('views',path.join(__dirname,'views'));
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
     const tit={};
     res.status(200).render('home.pug', tit
     )});
